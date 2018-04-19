c89915b40949347345ce84cf73921a499db6b0d0
Lazarocp <lazaro.cuenca8@gmail.com>
2018-04-19 17:33:26 +0200

    inicio

--- src/org/formacion/builder/Main.java
+++ src/org/formacion/builder/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main (String[] args) {
 
 		// este codigo debe compilar
-		/*
+		
 		Persona madre = new Persona.Builder("Maria")
                 .setMunicipio("Selva")
                 .setMayor(37)
@@ -16,13 +16,10 @@ public class Main {
                 .setMenor(4)
                 .setColegio("Colegio de Selva")
                 .build();
-		*/
-		// esta codigo NO debe compilar
-		/*
+		
 		Persona mal = new Persona.Builder("Luisa")
 				    .setMayor(20)
 				    .setColegio("Colegio de Villa Arriba")
 				    .build();
-        */
 	}
 }
